06685f39bcda220c7c8f8f1581e55c82d9aa4525
Andrzej Mazurek <amazurek@bink.com>
2019-10-22 17:44:45 +0100

    Add Cooperative producer with changed style due to tests

--- s/producer_cooperative.py
+++ s/producer_cooperative.py
@@ -49,13 +49,8 @@ class CooperativeProducer(object):
 
     errors_in_processes = []
 
-    loyalty_scheme = get(
-        models.LoyaltyScheme, query=[models.LoyaltyScheme.slug == LOYALTY_SCHEME_SLUG]
-    )
-    payment_provider = get(
-        models.PaymentProvider,
-        query=[models.PaymentProvider.slug == PAYMENT_PROVIDER_SLUG],
-    )
+    loyalty_scheme = get(models.LoyaltyScheme, query=[models.LoyaltyScheme.slug == LOYALTY_SCHEME_SLUG])
+    payment_provider = get(models.PaymentProvider, query=[models.PaymentProvider.slug == PAYMENT_PROVIDER_SLUG])
 
     def save_file(self, transactions, transactions_type):
         input_file_path = f"../harmonia/files/imports/cooperative/mock_transactions_{transactions_type}.dat"
@@ -103,13 +98,10 @@ class CooperativeProducer(object):
 
     def user_request_to_hermes(self):
         register_resp = requests.post(
-            f"{HERMES_URL}/users/register",
-            json={"email": f"{uuid4()}@txmatch.com", "password": "Password01"},
+            f"{HERMES_URL}/users/register", json={"email": f"{uuid4()}@txmatch.com", "password": "Password01"}
         )
         register_resp.raise_for_status()
-        user_register_headers = {
-            "Authorization": f"Token {register_resp.json()['api_key']}"
-        }
+        user_register_headers = {"Authorization": f"Token {register_resp.json()['api_key']}"}
         self.log.debug(f"User in Hermes has been registered: {register_resp.json()}")
         return user_register_headers
 
@@ -130,14 +122,10 @@ class CooperativeProducer(object):
                 "payment_card": 1,
             }
             pca_resp = requests.post(
-                f"{HERMES_URL}/payment_cards/accounts",
-                json=payment_card,
-                headers=user_register_headers,
+                f"{HERMES_URL}/payment_cards/accounts", json=payment_card, headers=user_register_headers
             )
             pca_resp.raise_for_status()
-            self.log.debug(
-                f"Payment card in Hermes has been registered {pca_resp.json()}"
-            )
+            self.log.debug(f"Payment card in Hermes has been registered {pca_resp.json()}")
         except Exception as error:
             self.errors_in_processes.append(error)
         return payment_card
@@ -150,19 +138,11 @@ class CooperativeProducer(object):
             headers=user_register_headers,
         )
         sa2_resp.raise_for_status()
-        self.log.debug(
-            f"Scheme account in Hermes has been registered {sa2_resp.json()}"
-        )
+        self.log.debug(f"Scheme account in Hermes has been registered {sa2_resp.json()}")
         return scheme_card, sa2_resp
 
-    def credentials_request_to_hermes(
-        self, sa2_resp, user_register_headers, scheme_card_number
-    ):
-        credentials_json = {
-            "password": "1234567",
-            "email": "email=bv@gmail.com",
-            "card_number": scheme_card_number,
-        }
+    def credentials_request_to_hermes(self, sa2_resp, user_register_headers, scheme_card_number):
+        credentials_json = {"password": "1234567", "email": "email=bv@gmail.com", "card_number": scheme_card_number}
         sa2_cred_resp = requests.put(
             f"{HERMES_URL}/schemes/accounts/{sa2_resp.json()['id']}/credentials",
             json=credentials_json,
@@ -172,16 +152,12 @@ class CooperativeProducer(object):
         json_status = {"journey": "link", "status": 1}
         headers = {"Authorization": "Token F616CE5C88744DD52DB628FAD8B3D"}
         status_resp = requests.post(
-            f"{HERMES_URL}/schemes/accounts/{sa2_resp.json()['id']}/status",
-            json=json_status,
-            headers=headers,
+            f"{HERMES_URL}/schemes/accounts/{sa2_resp.json()['id']}/status", json=json_status, headers=headers
         )
         status_resp.raise_for_status()
 
         status_resp = requests.post(
-            f"{HERMES_URL}/schemes/accounts/{sa2_resp.json()['id']}/status",
-            json=json_status,
-            headers=headers,
+            f"{HERMES_URL}/schemes/accounts/{sa2_resp.json()['id']}/status", json=json_status, headers=headers
         )
         status_resp.raise_for_status()
         self.log.debug(f"Credentials in Hermes has been registered: {credentials_json}")
@@ -205,15 +181,9 @@ class CooperativeProducer(object):
 
         for transaction in file["transactions"]:
             user_register_headers = self.user_request_to_hermes()
-            payment_card = self.payment_card_request_to_hermes(
-                user_register_headers, transaction
-            )
-            scheme_card_number, sa2_resp = self.scheme_account_request_to_hermes(
-                user_register_headers
-            )
-            self.credentials_request_to_hermes(
-                sa2_resp, user_register_headers, scheme_card_number
-            )
+            payment_card = self.payment_card_request_to_hermes(user_register_headers, transaction)
+            scheme_card_number, sa2_resp = self.scheme_account_request_to_hermes(user_register_headers)
+            self.credentials_request_to_hermes(sa2_resp, user_register_headers, scheme_card_number)
             self.payment_request_to_harmonia(transaction, payment_card)
 
 
