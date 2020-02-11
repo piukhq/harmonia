0b51425f731c956fc77f0a668555311c6151da4f
Andrzej Mazurek <amazurek@bink.com>
2020-02-11 12:31:22 +0000

    Add bink_loyalty export agent with new make_export_data method

--- app/exports/agents/bink_loyalty.py
+++ app/exports/agents/bink_loyalty.py
@@ -7,16 +7,22 @@ from app import models, db
 class BinkLoyalty(SingleExportAgent):
     provider_slug = "bink-loyalty"
 
-    def export(self, matched_transaction_id: int):
+    def make_export_data(self, matched_transaction_id):
         matched_transaction = db.session.query(models.MatchedTransaction).get(matched_transaction_id)
         self.log.info(f"{type(self).__name__} handling {matched_transaction}.")
 
         value = Decimal(matched_transaction.spend_amount) / Decimal(matched_transaction.spend_multiplier)
-        export_data = {
+        body = {
             "tid": matched_transaction.transaction_id,
             "value": f"{matched_transaction.spend_currency} {value.quantize(Decimal('0.01'))}",
             "card_number": matched_transaction.payment_transaction.user_identity.loyalty_id,
         }
+        return {"body": body, "matched_transaction": matched_transaction}
+
+    def export(self, export_data: dict) -> bool:
+        export_data = export_data["body"]
+        matched_transaction = export_data["matched_transaction"]
+
         self.log.info(f"Export: {export_data}")
 
         self.log.info(f"Marking {matched_transaction} as exported.")
