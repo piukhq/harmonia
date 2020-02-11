df6f0f43d2d74e7f6f1b17f61a2ef78c332a99cb
Andrzej Mazurek <amazurek@bink.com>
2020-02-11 09:51:42 +0000

    Add batch_export agent

--- app/exports/agents/bases/batch_export_agent.py
+++ app/exports/agents/bases/batch_export_agent.py
@@ -1,6 +1,8 @@
 from app.exports.agents import BaseAgent
 from app.scheduler import CronScheduler
 
+import settings
+
 
 class BatchExportAgent(BaseAgent):
     def run(self, *, once: bool = False):
@@ -18,6 +20,19 @@ class BatchExportAgent(BaseAgent):
         return
 
     def export_all(self, *, once: bool = False):
+        for export_data in self.yield_export_data():
+            if settings.EXPORT_TO_FILE:
+                self._save_to_file(export_data["body"])
+                return
+            else:
+                self.send_export_data(export_data)
+
+    def yield_export_data(self):
         raise NotImplementedError(
-            "Override the export_all method in your agent to act as the entry point into the batch export process."
+            "Override the yield_export_data method in your agent."
+        )
+
+    def send_export_data(self, export_data: dict):
+        raise NotImplementedError(
+            "Override the send_export_data method in your agent."
         )
