5e7409b1caa64002e7e28863baaa3b360b0d49b7
Chris Latham <backwardspy@gmail.com>
2020-03-23 15:59:39 +0000

    add queue-based import agent type

--- app/imports/agents/__init__.py
+++ app/imports/agents/__init__.py
@@ -2,4 +2,5 @@ from .bases.base import BaseAgent  # noqa
 from .bases.file_agent import FileAgent  # noqa
 from .bases.passive_api_agent import PassiveAPIAgent  # noqa
 from .bases.active_api_agent import ActiveAPIAgent  # noqa
+from .bases.queue_agent import QueueAgent  # noqa
 from .registry import import_agents  # noqa
