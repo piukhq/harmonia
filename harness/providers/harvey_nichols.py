4871215b5c08b56eb10a30b27db8cca802e6b29c
mlatham <ml@bink.com>
2020-06-02 17:37:01 +0100

    MER-13 harvey nichols matching.

--- harness/providers/harvey_nichols.py
+++ harness/providers/harvey_nichols.py
@@ -1,12 +1,23 @@
 import json
 from uuid import uuid4
+import typing as t
 
 from harness.providers.base import BaseImportDataProvider
 from app.currency import to_pounds
 
 
+def _get_card_scheme(slug: str) -> t.Tuple[int, str]:
+    return {
+        "amex": (1, "AMERICAN EXPRESS"),
+        "visa": (2, "VISA"),
+        "mastercard": (3, "MASTERCARD"),
+        "bink-payment": (9, "Bink-Payment"),
+    }[slug]
+
+
 class HarveyNichols(BaseImportDataProvider):
     def provide(self, fixture: dict) -> bytes:
+        scheme_id, scheme_name = _get_card_scheme(fixture["payment_provider"]["slug"])
         return json.dumps(
             {
                 "transactions": [
@@ -16,7 +27,7 @@ class HarveyNichols(BaseImportDataProvider):
                             "first_6": user["first_six"],
                             "last_4": user["last_four"],
                             "expiry": "0",
-                            "scheme": "AMEX",
+                            "scheme": scheme_name,
                         },
                         "amount": {"value": to_pounds(transaction["amount"]), "unit": "GBP"},
                         "store_id": "0001017   005682",
