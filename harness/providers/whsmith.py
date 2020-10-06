4dad67329182ae05cfcda73f6ccf922ae5f8967b
Stewart Perrygrove <sperrygrove@bink.com>
2020-10-06 11:12:33 +0100

    Round whsmith tx time in the harness provider

--- harness/providers/whsmith.py
+++ harness/providers/whsmith.py
@@ -19,13 +19,19 @@ def _get_card_type(slug: str) -> str:
     return providers[slug]
 
 
+def rounded_transaction_date(transaction_date):
+    dt = pendulum.instance(transaction_date).in_tz("Europe/London")
+    rounded = dt.replace(minute=dt.minute + (1 if dt.second >= 30 else 0), second=0)
+    return rounded.format("YYYY-MM-DDTHH:mm:ss.SSS")
+
+
 class WhSmith(BaseImportDataProvider):
     def provide(self, fixture: dict) -> bytes:
         transactions = [
             (
                 str(uuid4()),
                 "5842003682292310000",
-                pendulum.instance(transaction["date"]).in_tz("UTC").format("YYYY-MM-DDTHH:mm:ss.SSS"),
+                rounded_transaction_date(transaction["date"]),
                 "1579532400",
                 fixture["store_id"],
                 "Reading",
