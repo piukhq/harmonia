babc3458e2569fd0f576ac7f16b166516cafc973
David Felce <dfelce@bink.com>
2020-11-10 14:41:37 +0000

    MER-661: batching/chunks for commits

--- harness/bulk_load_db.py
+++ harness/bulk_load_db.py
@@ -21,6 +21,7 @@ EXPORT_TRANSACTION_COUNT = 10
 PENDING_EXPORT_COUNT = 10
 FILE_SEQUENCE_COUNT = 5
 MAX_PROCESSES = 4
+BATCHSIZE = 1000
 
 
 def create_primary_records():
@@ -42,34 +43,58 @@ def create_primary_records():
     session.commit()
 
 
-def create_scheme_transactions(scheme_transaction_count: int):
+def get_batch_chunks(transaction_count, batchsize):
+    while transaction_count > 0:
+        if transaction_count >= batchsize:
+            yield batchsize
+        else:
+            yield transaction_count
+        transaction_count -= batchsize
+
+
+def create_scheme_transactions(scheme_transaction_count: int, batchsize: int):
     # Create random scheme transactions
     click.secho(
         f"Creating {scheme_transaction_count} scheme transactions", fg="cyan", bold=True,
     )
-    app_factories.SchemeTransactionFactory.create_batch(scheme_transaction_count)
-    session.commit()
+    # create batches in chunks of batchsize records, then commit
+    chunks = get_batch_chunks(scheme_transaction_count, batchsize=batchsize)
+    for chunk in chunks:
+        click.secho(
+            f"Creating chunk of {chunk} scheme transactions", fg="cyan", bold=True,
+        )
+        app_factories.SchemeTransactionFactory.create_batch(chunk)
+        session.commit()
 
 
-def create_import_transactions(import_transaction_count: int):
+def create_import_transactions(import_transaction_count: int, batchsize: int):
     # Create random import transactions
     click.secho(
         f"Creating {import_transaction_count} import transactions", fg="cyan", bold=True,
     )
-    imports_factories.ImportTransactionFactory.create_batch(import_transaction_count)
-    session.commit()
+    chunks = get_batch_chunks(import_transaction_count, batchsize=batchsize)
+    for chunk in chunks:
+        click.secho(
+            f"Creating chunk of {chunk} scheme transactions", fg="cyan", bold=True,
+        )
+        imports_factories.ImportTransactionFactory.create_batch(chunk)
+        session.commit()
 
 
 def bulk_load_db(
-    scheme_transaction_count: int, import_transaction_count: int, skip_primary: bool, max_processes: int,
+    scheme_transaction_count: int,
+    import_transaction_count: int,
+    transactions_only: bool,
+    max_processes: int,
+    batchsize: int,
 ):
     # Create our primary records
-    if not skip_primary:
+    if not transactions_only:
         create_primary_records()
 
-    # Create payment transactions, linking to our users in the primary table
-    app_factories.PaymentTransactionFactory.create_batch(PAYMENT_TRANSACTION_COUNT)
-    session.commit()
+        # Create payment transactions, linking to our users in the primary table
+        app_factories.PaymentTransactionFactory.create_batch(PAYMENT_TRANSACTION_COUNT)
+        session.commit()
 
     # These two tables (import_transactions and scheme_transactions) are the big ones and can be asynchronously
     # appended to. import_transactions is a standalone table and data can just be pushed into it
@@ -77,31 +102,39 @@ def bulk_load_db(
     create_import_transactions_executor = ProcessPoolExecutor(max_workers=max_processes)
     import_transactions_max_processes = int(max_processes / 2)  # There are 2 tables to divide the processes between
     import_transactions_per_process = int(import_transaction_count / import_transactions_max_processes)
-    # Create the param for the task
+    # Create the params for the task
     import_transaction_counts = [import_transactions_per_process for x in range(import_transactions_max_processes)]
-    create_import_transactions_executor.map(create_import_transactions, import_transaction_counts)
+    import_transaction_batchsizes = [batchsize for x in range(len(import_transaction_counts))]
+    create_import_transactions_executor.map(
+        create_import_transactions, import_transaction_counts, import_transaction_batchsizes
+    )
     # scheme_transactions provides foreign key links for following tables, so we need to wait for it to
     # complete by using a context manager
     with ProcessPoolExecutor(max_workers=max_processes) as create_scheme_transactions_executor:
         scheme_transactions_max_processes = int(max_processes / 2)  # There are 2 tables to divide the processes between
         scheme_transactions_per_process = int(scheme_transaction_count / scheme_transactions_max_processes)
-        # Create the param for the task
+        # Create the params for the task
         scheme_transaction_counts = [scheme_transactions_per_process for x in range(scheme_transactions_max_processes)]
-        create_scheme_transactions_executor.map(create_scheme_transactions, scheme_transaction_counts)
+        scheme_transaction_batchsizes = [batchsize for x in range(len(scheme_transaction_counts))]
+        create_scheme_transactions_executor.map(
+            create_scheme_transactions, scheme_transaction_counts, scheme_transaction_batchsizes
+        )
 
-    # Create random matched transactions. This table relies on merchant_identifier, scheme_transaction
-    # and payment_transaction rows being in place
-    app_factories.MatchedTransactionFactory.create_batch(MATCHED_TRANSACTION_COUNT)
-    session.commit()
-    # Create random export transactions
-    exports_factories.ExportTransactionFactory.create_batch(EXPORT_TRANSACTION_COUNT)
-    session.commit()
-    # Create random pending exports
-    exports_factories.PendingExportFactory.create_batch(PENDING_EXPORT_COUNT)
-    session.commit()
-    # Create random file sequence records
-    exports_factories.FileSequenceNumberFactory.create_batch(FILE_SEQUENCE_COUNT)
-    session.commit()
+    # The remaining tables
+    if not transactions_only:
+        # Create random matched transactions. This table relies on merchant_identifier, scheme_transaction
+        # and payment_transaction rows being in place
+        app_factories.MatchedTransactionFactory.create_batch(MATCHED_TRANSACTION_COUNT)
+        session.commit()
+        # Create random export transactions
+        exports_factories.ExportTransactionFactory.create_batch(EXPORT_TRANSACTION_COUNT)
+        session.commit()
+        # Create random pending exports
+        exports_factories.PendingExportFactory.create_batch(PENDING_EXPORT_COUNT)
+        session.commit()
+        # Create random file sequence records
+        exports_factories.FileSequenceNumberFactory.create_batch(FILE_SEQUENCE_COUNT)
+        session.commit()
 
 
 @click.command()
@@ -112,21 +145,28 @@ def bulk_load_db(
     "--import-transaction-count", type=click.INT, default=IMPORT_TRANSACTION_COUNT, show_default=True, required=False
 )
 @click.option(
-    "--skip-primary", is_flag=True, help="Skip creation of base table records", show_default=True, default=False
+    "--transactions-only",
+    is_flag=True,
+    help="Only append new records to the two big transaction tables",
+    show_default=True,
+    default=False,
 )
 @click.option("--max-processes", type=click.INT, default=MAX_PROCESSES, show_default=True, required=False)
+@click.option("--batchsize", type=click.INT, default=BATCHSIZE, show_default=True, required=False)
 def main(
     scheme_transaction_count: int,
     import_transaction_count: int,
-    skip_primary: bool,
+    transactions_only: bool,
     max_processes: int,
+    batchsize: int,
 ):
 
     bulk_load_db(
         scheme_transaction_count=scheme_transaction_count,
         import_transaction_count=import_transaction_count,
-        skip_primary=skip_primary,
+        transactions_only=transactions_only,
         max_processes=max_processes,
+        batchsize=batchsize,
     )
 
 
