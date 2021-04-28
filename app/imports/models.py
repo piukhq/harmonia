3f4efed14b56071d4dabcae1945f05ff0ee584a8
Chris Latham <backwardspy@gmail.com>
2021-04-28 11:31:46 +0100

    make transaction_id field length on import & export transaction consistent

--- app/imports/models.py
+++ app/imports/models.py
@@ -8,7 +8,7 @@ class ImportTransaction(Base, ModelMixin):
     __tablename__ = "import_transaction"
     __table_args__ = (s.UniqueConstraint("provider_slug", "transaction_id", name="_slug_tid_it_uc"),)
 
-    transaction_id = s.Column(s.String(50), nullable=False)
+    transaction_id = s.Column(s.String(100), nullable=False)
     provider_slug = s.Column(s.String(50), nullable=False)
     identified = s.Column(s.Boolean, nullable=False)
     match_group = s.Column(s.String(36), nullable=False)
