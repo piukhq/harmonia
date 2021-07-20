fc22f869a1344c6d7cde3f779a7ca408c90b2c27
Chris Latham <backwardspy@gmail.com>
2021-07-20 16:21:32 +0100

    fix wasabi agent's usage of time

--- app/matching/agents/wasabi.py
+++ app/matching/agents/wasabi.py
@@ -1,6 +1,7 @@
 import typing as t
 from datetime import datetime, time
 
+import pendulum
 from sqlalchemy.orm.query import Query
 
 from app import models
@@ -8,56 +9,68 @@ from app.matching.agents.base import BaseMatchingAgent, MatchResult
 
 
 class Wasabi(BaseMatchingAgent):
-    def _filter_with_auth_code(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
+    time_tolerance = 60 * 3
+
+    def _query_by_auth_code(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
+        """
+        * Query: Scheme + MID + Date + Amount + (Auth Code | Time)
+        * Fallback: Time, PAN F6L4
+        """
+        scheme_transactions = scheme_transactions.filter(
+            models.SchemeTransaction.payment_provider_slug == self.payment_transaction.provider_slug,
+            models.SchemeTransaction.spend_amount == self.payment_transaction.spend_amount,
+        )
+
         if self.payment_transaction.auth_code:
             scheme_transactions = scheme_transactions.filter(
-                models.SchemeTransaction.auth_code == self.payment_transaction.auth_code
+                models.SchemeTransaction.auth_code == self.payment_transaction.auth_code,
+                models.SchemeTransaction.transaction_date.between(
+                    *(
+                        datetime.combine(self.payment_transaction.transaction_date.date(), time_parts).isoformat()
+                        for time_parts in (time.min, time.max)
+                    )
+                ),
             )
+        else:
+            scheme_transactions = self._time_filter(scheme_transactions, tolerance=self.time_tolerance)
 
         match, multiple_returned = self._check_for_match(scheme_transactions)
 
         if multiple_returned:
-            match = self._filter_other(scheme_transactions)
+            match = self._filter(scheme_transactions.all(), [self._filter_by_time, self._filter_by_card_number])
 
         return match
 
-    def _filter_amex(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
+    def _query_by_dpan(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
+        """
+        * Query: Scheme + MID + Date + Amount + Time + [PAN F6L4]
+        * Fallback: PAN F6L4 (if no DPAN)
+        """
+        scheme_transactions = scheme_transactions.filter(
+            models.SchemeTransaction.payment_provider_slug == self.payment_transaction.provider_slug,
+            models.SchemeTransaction.spend_amount == self.payment_transaction.spend_amount,
+        )
+        scheme_transactions = self._time_filter(scheme_transactions, tolerance=self.time_tolerance)
+
         # dpan is an optional field that we use if we have it
-        if self.payment_transaction.first_six and self.payment_transaction.last_four:
+        if self.payment_transaction.first_six:
             scheme_transactions = scheme_transactions.filter(
                 models.SchemeTransaction.first_six == self.payment_transaction.first_six,
                 models.SchemeTransaction.last_four == self.payment_transaction.last_four,
             )
 
-        return self._filter_other(scheme_transactions)
-
-    def _filter_other(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
-        scheme_transactions = self._time_filter(scheme_transactions, tolerance=60 * 3)
         match, multiple_returned = self._check_for_match(scheme_transactions)
 
-        # we only want to filter by card number if the dpan isn't present.
+        # we only apply fallback filters if we didn't get dpan
         if multiple_returned and not self.payment_transaction.first_six:
             match = self._filter(scheme_transactions.all(), [self._filter_by_card_number])
 
         return match
 
     def _filter_scheme_transactions(self, scheme_transactions: Query) -> t.Optional[models.SchemeTransaction]:
-        transaction_date = self.payment_transaction.transaction_date
-        scheme_transactions = scheme_transactions.filter(
-            models.SchemeTransaction.spend_amount == self.payment_transaction.spend_amount,
-            models.SchemeTransaction.payment_provider_slug == self.payment_transaction.provider_slug,
-            models.SchemeTransaction.transaction_date.between(
-                *(
-                    datetime.combine(transaction_date.date(), time_parts).isoformat()
-                    for time_parts in (time.min, time.max)
-                )
-            ),
-        )
-        return {
-            "visa": self._filter_with_auth_code,
-            "amex": self._filter_amex,
-            "mastercard": self._filter_with_auth_code,
-        }[self.payment_transaction.provider_slug](scheme_transactions)
+        return {"visa": self._query_by_auth_code, "amex": self._query_by_dpan, "mastercard": self._query_by_auth_code}[
+            self.payment_transaction.provider_slug
+        ](scheme_transactions)
 
     def _filter_by_card_number(
         self, scheme_transactions: t.List[models.SchemeTransaction]
@@ -68,6 +81,17 @@ class Wasabi(BaseMatchingAgent):
             transaction
             for transaction in scheme_transactions
             if (transaction.first_six == user_identity.first_six and transaction.last_four == user_identity.last_four)
+        ]
+        return matched_transactions
+
+    def _filter_by_time(
+        self, scheme_transactions: t.List[models.SchemeTransaction]
+    ) -> t.List[models.SchemeTransaction]:
+        date = pendulum.instance(self.payment_transaction.transaction_date)
+        before = date.subtract(seconds=self.time_tolerance)
+        after = date.add(seconds=self.time_tolerance)
+        matched_transactions = [
+            transaction for transaction in scheme_transactions if before <= transaction.transaction_date <= after
         ]
         return matched_transactions
 
