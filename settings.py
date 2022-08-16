4a1118e2c5a168d8b8632c9a86e082179693a7da
Thenuja Viknarajah <61195906+thenu97@users.noreply.github.com>
2022-08-16 08:44:48 +0100

    add application name to db (#73)

--- settings.py
+++ settings.py
@@ -84,6 +84,8 @@ else:
         ]
     )
 
+POSTGRES_CONNECT_ARGS = {"application_name": "harmonia"}
+
 # Connection details for Redis.
 # Redis is used as a configuration store that can be updated at runtime.
 REDIS_URL = getenv("TXM_REDIS_URL")
