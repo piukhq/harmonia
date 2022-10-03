1a24997238b4b2c68284a0754a68d3fb464590b2
Michal Jozwiak <mjozwiak@bink.com>
2022-10-03 14:00:30 +0100

    merge migrations

---
+++ alembic/versions/2bdcf59aedfd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2bdcf59aedfd
+Revises: 207865517a70, 76de66565f46
+Create Date: 2022-10-03 12:59:36.283498+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2bdcf59aedfd'
+down_revision = ('207865517a70', '76de66565f46')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
