c092ff009516e637e10698f9693fccc012873ba6
carla <cgouws@bink.com>
2022-10-20 18:40:34 +0100

    Initial harness/bulk_load_db.py run successful; use of --skip-base-tables freezes process

--- harness/factories/app/exports/factories.py
+++ harness/factories/app/exports/factories.py
@@ -1,7 +1,7 @@
 import factory
 
 from app import models
-from app.exports.models import ExportTransaction, FileSequenceNumber, PendingExport
+from app.exports.models import ExportTransaction, FileSequenceNumber, PendingExport, ExportTransactionStatus
 from harness.factories.common import generic, session
 
 
@@ -13,8 +13,18 @@ class ExportTransactionFactory(factory.alchemy.SQLAlchemyModelFactory):
 
     transaction_id = factory.LazyAttribute(lambda o: generic.text.random.randstr(unique=True, length=50))
     provider_slug = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
-    destination = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=500))
-    data = factory.LazyAttribute(lambda o: generic.json_provider.json())
+    transaction_date = factory.LazyAttribute(lambda o: generic.transaction_date_provider.transaction_date(days=30))
+    spend_amount = factory.LazyAttribute(lambda o: generic.numeric.integer_number(start=1))
+    spend_currency = factory.LazyAttribute(lambda o: generic.finance.currency_iso_code(allow_random=True))
+    loyalty_id = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
+    mid = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
+    primary_identifier = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
+    user_id = factory.LazyAttribute(lambda o: generic.numeric.integer_number(start=1))
+    scheme_account_id = factory.LazyAttribute(lambda o: generic.numeric.integer_number(start=1))
+    credentials = factory.LazyAttribute(lambda o: generic.cryptographic.token_urlsafe())
+    auth_code = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=20))
+    approval_code = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=20))
+    status = factory.LazyAttribute(lambda o: generic.choice(items=[x for x in ExportTransactionStatus]))
 
 
 class PendingExportFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -23,11 +33,12 @@ class PendingExportFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = session
         sqlalchemy_session_persistence = None
 
-    def matched_transaction():
-        yield from session.query(models.MatchedTransaction).limit(500).all()
+    def export_transaction():
+        yield from session.query(models.ExportTransaction).limit(500).all()
 
     provider_slug = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
-    matched_transaction = factory.iterator(matched_transaction)
+    export_transaction = factory.iterator(export_transaction)
+    retry_count = factory.LazyAttribute(lambda o: generic.numeric.integer_number(start=0, end=3))
 
 
 class FileSequenceNumberFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -37,4 +48,4 @@ class FileSequenceNumberFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session_persistence = None
 
     provider_slug = factory.LazyAttribute(lambda o: generic.text.random.randstr(length=50))
-    next_value = factory.LazyAttribute(lambda o: generic.numbers.integer_number(start=1))
+    next_value = factory.LazyAttribute(lambda o: generic.numeric.integer_number(start=1))
