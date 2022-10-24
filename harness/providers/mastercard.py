29783f6ba92ef4f9ed3a526732e347321f9987f2
carla <cgouws@bink.com>
2022-10-24 12:14:57 +0100

    Remove references to TS44

--- harness/providers/mastercard.py
+++ harness/providers/mastercard.py
@@ -1,6 +1,5 @@
 import string
 import typing as t
-from random import randint
 
 import pendulum
 
@@ -16,55 +15,6 @@ WidthField = t.Tuple[t.Any, int]
 
 def join(*args: WidthField) -> str:
     return "".join(str(value).ljust(length) for value, length in args)
-
-
-class MastercardTS44Settlement(BaseImportDataProvider):
-    def provide(self, fixture: dict) -> bytes:
-        now = pendulum.now()
-        lines = []
-
-        # header
-        lines.append(
-            join(
-                ("H", 1),  # header record
-                (now.format("YYYYMMDD"), 8),
-                (now.format("hhmmss"), 6),
-                ("00000017597", 11),  # member ICA
-                ("", 174),  # filler
-            )
-        )
-
-        lines.extend(
-            join(
-                ("D", 1),  # data record
-                (str(randint(0, 10**12)).rjust(13, "0"), 13),  # tx sequence number
-                ("", 19),  # bank account number
-                (str(to_pounds(transaction["amount"])).rjust(13, "0"), 13),
-                (pendulum.instance(transaction["date"]).in_tz("Europe/London").format("YYYYMMDD"), 8),
-                (fixture["loyalty_scheme"]["slug"].upper(), 60),
-                (transaction["identifier"], 22),
-                (str(randint(0, 10**8)).rjust(9, "0"), 9),  # location ID
-                (str(randint(0, 10**5)).rjust(6, "0"), 6),  # issuer ICA code
-                (pendulum.instance(transaction["date"]).in_tz("Europe/London").format("HHmm"), 4),  # transaction time
-                (transaction["settlement_key"][:9], 9),  # banknet ref number
-                (user["token"], 30),  # bank customer number
-                (str(randint(0, 10**5)).rjust(6, "0"), 6),  # aggregate merchant ID
-            )
-            for user in fixture["users"]
-            for transaction in user["transactions"]
-        )
-
-        # trailer
-        lines.append(
-            join(
-                ("T", 1),  # trailer record
-                (str(len(lines) - 1).rjust(12, "0"), 12),  # record count
-                ("00000017597", 11),  # member ICA
-                ("", 176),  # filler
-            )
-        )
-
-        return "\n".join(lines).encode()
 
 
 class MastercardTGX2Settlement(BaseImportDataProvider):
--- harness/providers/registry.py
+++ harness/providers/registry.py
@@ -1,4 +1,3 @@
-import settings
 from app.registry import Registry
 from harness.providers.base import BaseImportDataProvider
 
@@ -8,12 +7,8 @@ import_data_providers.add("iceland-bonus-card", "harness.providers.iceland.Icela
 import_data_providers.add("amex-settlement", "harness.providers.amex.AmexSettlement")
 import_data_providers.add("amex-auth", "harness.providers.amex.AmexAuth")
 import_data_providers.add("mastercard-auth", "harness.providers.mastercard.MastercardAuth")
+import_data_providers.add("mastercard-settled", "harness.providers.mastercard.MastercardTGX2Settlement")
 import_data_providers.add("visa-auth", "harness.providers.visa.VisaAuth")
 import_data_providers.add("visa-settlement", "harness.providers.visa.VisaSettlement")
 import_data_providers.add("visa-refund", "harness.providers.visa.VisaRefund")
 import_data_providers.add("wasabi-club", "harness.providers.wasabi.Wasabi")
-
-if settings.MASTERCARD_TGX2_ENABLED:
-    import_data_providers.add("mastercard-settled", "harness.providers.mastercard.MastercardTGX2Settlement")
-else:
-    import_data_providers.add("mastercard-settled", "harness.providers.mastercard.MastercardTS44Settlement")
