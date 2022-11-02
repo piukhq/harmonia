eb2cb806ab900b81c36f1bbbdf6d598a1a2b387b
carla <cgouws@bink.com>
2022-11-02 10:08:50 +0000

    Add test for current behaviour in Squaremeal export

--- tests/test_export_squaremeal.py
+++ tests/test_export_squaremeal.py
@@ -2,7 +2,9 @@ import pendulum
 import responses
 
 from app import db, models
+from app.exports.agents import AgentExportData, AgentExportDataOutput
 from app.exports.agents.squaremeal import SquareMeal
+from app.exports.models import ExportTransaction
 from app.feeds import FeedType
 
 
@@ -40,12 +42,12 @@ def create_export_transaction(transaction_id, merchant_identifier, settlement_ke
         loyalty_id=merchant_identifier,
         mid="1234567",
         provider_slug="squaremeal",
-        transaction_date=pendulum.now().in_timezone("Europe/London"),
+        transaction_date=pendulum.DateTime(2022, 11, 1, 17, 14, 8, 838138, tzinfo=pendulum.timezone("Europe/London")),
         spend_amount=5566,
         spend_currency="GBP",
         payment_card_account_id=1,
         feed_type=FeedType.AUTH,
-        settlement_key=None,
+        settlement_key=settlement_key,
         user_id=1,
         scheme_account_id=1,
         credentials="something",
@@ -74,3 +76,70 @@ def test_get_settlement_key_with_settlement_key(db_session: db.Session) -> None:
     settlement_key = squaremeal.get_settlement_key(exp_txn, db_session)
 
     assert settlement_key == expected_settlement_key
+
+
+def test_make_export_data(db_session: db.Session):
+    exp_txn = create_export_transaction("1234567", 10, "123456")
+    squaremeal = SquareMeal()
+
+    expected_result = AgentExportData(
+        outputs=[
+            AgentExportDataOutput(
+                key="export.json",
+                data={
+                    "transaction_id": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+                    "loyalty_id": 10,
+                    "auth": True,
+                    "cleared": False,
+                    "mid": "1234567",
+                    "transaction_date": "2022-11-01T17:14:08",
+                    "transaction_amount": 5566,
+                    "transaction_currency": "GBP",
+                    "payment_card_account_id": 1,
+                    "store_id": None,
+                    "brand_id": None,
+                    "payment_card_last_four": None,
+                    "payment_scheme": {"slug": None, "auth_code": None, "approval_code": None},
+                    "payment_card_expiry_month": None,
+                    "payment_card_expiry_year": None,
+                },
+            )
+        ],
+        transactions=[
+            ExportTransaction(
+                id=None,
+                created_at=None,
+                updated_at=None,
+                transaction_id="1234567",
+                feed_type=FeedType.AUTH,
+                provider_slug="squaremeal",
+                transaction_date=pendulum.DateTime(
+                    2022, 11, 1, 17, 14, 8, 838138, tzinfo=pendulum.timezone("Europe/London")
+                ),
+                spend_amount=5566,
+                spend_currency="GBP",
+                loyalty_id=10,
+                mid="1234567",
+                primary_identifier="test-mid-primary",
+                location_id=None,
+                merchant_internal_id=None,
+                user_id=1,
+                scheme_account_id=1,
+                payment_card_account_id=1,
+                credentials="something",
+                auth_code=None,
+                approval_code=None,
+                status=None,
+                settlement_key="123456",
+                last_four=None,
+                expiry_month=None,
+                expiry_year=None,
+                payment_provider_slug=None,
+            )
+        ],
+        extra_data={},
+    )
+
+    result = squaremeal.make_export_data(exp_txn, db_session)
+
+    assert result.outputs == expected_result.outputs
