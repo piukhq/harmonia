b4ed624bd64a6b30d136ac9fd5adf666dd30a705
Michal Jozwiak <jozw.michal@gmail.com>
2022-12-02 12:59:17 +0000

    lowercase payment_provider in views (#103)
    * lowercase payment_provider in views
    
    * add slug validation to onboard endpoint
    
    * fix regex
    
    * ignore regex in pipeline
    
    * black
    
    * fix regex
    
    * add validation arg
    
    * fix validation
    
    * fix tests
    
    * remove not blank validation
    
    Co-authored-by: Michal Jozwiak <mjozwiak@bink.com>

--- app/identifiers/schemas.py
+++ app/identifiers/schemas.py
@@ -10,8 +10,8 @@ NotBlank = validate.Length(min=1)
 class IdentifierCreationSchema(Schema):
     identifier = fields.String(required=True, validate=NotBlank)
     identifier_type = fields.String(required=True, validate=validate.OneOf(IdentifierType._member_map_.keys()))
-    loyalty_plan = fields.String(required=True, validate=NotBlank)
-    payment_scheme = fields.String(required=True, validate=NotBlank)
+    loyalty_plan = fields.String(required=True, validate=validate.Regexp("^[a-z0-9]+(?:-[a-z0-9]+)*$"))
+    payment_scheme = fields.String(required=True, validate=validate.Regexp("^[a-z0-9]+(?:-[a-z0-9]+)*$"))
     location_id = fields.String(required=False, validate=NotBlank)
     merchant_internal_id = fields.String(required=False, validate=NotBlank)
 
