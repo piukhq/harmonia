ed06f8b76a0ace37fd724deb30b78b2617daa6d0
Michal Jozwiak <mjozwiak@bink.com>
2023-02-08 14:36:36 +0000

    fix tests

--- tests/test_service_atlas.py
+++ tests/test_service_atlas.py
@@ -1,9 +1,11 @@
 import logging
+from datetime import datetime
 from unittest import mock
 
 import pendulum
 from requests.models import Response
 
+from app.feeds import FeedType
 from app.reporting import get_logger
 from app.service import atlas
 
@@ -27,6 +29,16 @@ class MockExportTransaction:
     user_id = 10
     scheme_account_id = 2
     loyalty_id = "876543"
+    created_at = datetime(year=1999, month=9, day=26)
+    spend_currency = "CNY"
+    feed_type = FeedType.SETTLED
+    location_id = ""
+    merchant_internal_id = ""
+    payment_card_account_id = ""
+    settlement_key = "133"
+    auth_code = "123"
+    approval_code = "123"
+    export_uid = "123"
 
     def __init__(self, transaction_date=pendulum.now()):
         self.transaction_date = transaction_date
@@ -43,7 +55,7 @@ request_body = (
 )
 
 
-@mock.patch("app.service.queue.add", autospec=True)
+@mock.patch("app.service.exchange.publish", autospec=True)
 def test_queue_audit_message(mocked_queue):
     dt = pendulum.now()
     request_timestamp = pendulum.now().to_datetime_string()
@@ -66,14 +78,30 @@ def test_queue_audit_message(mocked_queue):
 
     assert audit_message == {
         "provider_slug": "test-slug",
+        "retry_count": 0,
         "transactions": [
             {
+                "approval_code": "123",
+                "authorisation_code": "123",
+                "encrypted_credentials": {"card_number": "loyalty-123", "merchant_identifier": "876543"},
+                "event_date_time": "1999-09-26T00:00:00",
+                "export_uid": "123",
+                "feed_type": "SETTLED",
+                "location_id": "",
+                "loyalty_id": 125,
+                "loyalty_identifier": "customer-number",
+                "merchant_internal_id": "",
+                "mid": "1234567",
+                "payment_card_account_id": "",
+                "record_uid": None,
+                "scheme_account_id": 2,
+                "settlement_key": "133",
+                "spend_amount": 1500,
+                "spend_currency": "CNY",
+                "status": "EXPORTED",
+                "transaction_date": dt.to_datetime_string(),
                 "transaction_id": 125,
                 "user_id": 10,
-                "spend_amount": 1500,
-                "transaction_date": dt.to_datetime_string(),
-                "loyalty_identifier": "customer-number",
-                "record_uid": None,
             }
         ],
         "audit_data": {
@@ -97,7 +125,7 @@ def test_queue_audit_message(mocked_queue):
     assert mocked_queue.call_args[0][0] == audit_message
 
 
-@mock.patch("app.service.queue.add", autospec=True)
+@mock.patch("app.service.exchange.publish", autospec=True)
 def test_queue_problems_exceptions_handled(mocked_queue, caplog):
     mocked_queue.side_effect = Exception("test exception")
     log.propagate = True
