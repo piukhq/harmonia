9eba03110b34ccfb6566a9a2b1f09090df6c87fd
Michal Jozwiak <mjozwiak@bink.com>
2023-07-20 12:11:23 +0100

    add itsu agent to harmonia

---
+++ app/exports/agents/itsu.py
@@ -0,0 +1,99 @@
+import hashlib
+import uuid
+
+import pendulum
+
+from app import db, models
+from app.config import KEY_PREFIX, Config, ConfigValue
+from app.exports.agents.bases.base import AgentExportData, AgentExportDataOutput
+from app.exports.agents.bases.singular_export_agent import SingularExportAgent
+from app.service import atlas
+from app.service.itsu import ItsuApi
+
+PROVIDER_SLUG = "itsu"
+
+BASE_URL_KEY = f"{KEY_PREFIX}exports.agents.{PROVIDER_SLUG}.base_url"
+
+
+class Itsu(SingularExportAgent):
+
+    provider_slug = PROVIDER_SLUG
+
+    config = Config(ConfigValue("base_url", key=BASE_URL_KEY, default="http://localhost"))
+
+    def __init__(self):
+        super().__init__()
+        self.api_class = ItsuApi
+
+        # Set up Prometheus metric types
+        self.prometheus_metrics = {
+            "counters": ["requests_sent", "failed_requests", "transactions"],
+            "histograms": ["request_latency"],
+        }
+
+    @staticmethod
+    def get_loyalty_identifier(export_transaction: models.ExportTransaction) -> str:
+        return hashlib.sha1("Bink-Itsu-" f"{export_transaction.decrypted_credentials['email']}".encode()).hexdigest()
+
+    def make_export_data(self, export_transaction: models.ExportTransaction, session: db.Session) -> AgentExportData:
+        dt = pendulum.instance(export_transaction.transaction_date)
+        return AgentExportData(
+            outputs=[
+                AgentExportDataOutput(
+                    "export.json",
+                    {
+                        "OrderID": str(uuid.uuid4()),
+                        "SubTransactions": [
+                            {
+                                "CustomerDetails": {
+                                    "MemberNumber": export_transaction.decrypted_credentials["card_number"],
+                                    "ExternalIdentifier": {"ExternalID": "", "ExternalSource": ""},
+                                },
+                                "TotalAmount": export_transaction.spend_amount,
+                                "PaidAmount": export_transaction.spend_amount,
+                                "OrderStatusID": 1,
+                                "OrderTypeID": 1,
+                                "OrderChannelID": 1,
+                                "OrderItems": [
+                                    {
+                                        "ItemID": "1",
+                                        "ItemName": "Bink Transaction",
+                                        "ItemPrice": export_transaction.spend_amount,
+                                    }
+                                ],
+                            }
+                        ],
+                        "OrderDate": dt.in_timezone("Europe/London").format("YYYY-MM-DDTHH:mm:ss"),
+                        "Location": {"ActeolSiteID": export_transaction.location_id},
+                        "Source": "BINK",
+                    },
+                )
+            ],
+            transactions=[export_transaction],
+            extra_data={"credentials": export_transaction.decrypted_credentials},
+        )
+
+    def export(self, export_data: AgentExportData, *, retry_count: int = 0, session: db.Session):
+        body: dict
+        _, body = export_data.outputs[0]  # type: ignore
+        api = self.api_class(self.config.get("base_url", session=session))
+        request_timestamp = pendulum.now().to_datetime_string()
+        endpoint = "api/Transaction/PostOrder"
+        response = api.post_matched_transaction(body, endpoint)
+        response_timestamp = pendulum.now().to_datetime_string()
+
+        request_url = api.base_url + endpoint
+        atlas.queue_audit_message(
+            atlas.make_audit_message(
+                self.provider_slug,
+                atlas.make_audit_transactions(
+                    export_data.transactions, tx_loyalty_ident_callback=self.get_loyalty_identifier
+                ),
+                request=body,
+                request_timestamp=request_timestamp,
+                response=response,
+                response_timestamp=response_timestamp,
+                request_url=request_url,
+                retry_count=retry_count,
+            )
+        )
---
+++ app/service/itsu.py
@@ -0,0 +1,96 @@
+import json
+import os
+from pathlib import Path
+from urllib.parse import urlencode, urljoin
+
+import arrow
+import requests
+from user_auth_token import UserTokenStore
+
+import settings
+from app.core.requests_retry import requests_retry_session
+from app.reporting import get_logger
+
+log = get_logger("itsu")
+
+ITSU_SECRET_KEY = "itsu-outbound-compound-key-join"
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class ItsuApi:
+    def __init__(self, base_url: str) -> None:
+        self.base_url = base_url
+        self.session = requests_retry_session()
+        self.oauth_token_timeout = 75600  # n_seconds in 21 hours
+        self.token_store = UserTokenStore(settings.REDIS_URL)
+        self.scheme_slug = "itsu"
+
+    @staticmethod
+    def _read_secret(key: str) -> str:
+        try:
+            root_path = Path(ROOT_DIR).parents[0]
+            path = Path(os.path.join(root_path, "mnt/secrets")) / key
+            with path.open() as f:
+                return json.loads(f.read())
+        except FileNotFoundError as e:
+            log.exception(e)
+            raise
+
+    def _store_token(self, token: str, current_timestamp: int) -> None:
+        token_dict = {
+            f"{self.scheme_slug.replace('-', '_')}_access_token": token,
+            "timestamp": current_timestamp,
+        }
+        self.token_store.set(scheme_account_id=self.scheme_slug, token=json.dumps(token_dict))
+
+    def _refresh_token(self) -> str:
+        credentials = self._read_secret(ITSU_SECRET_KEY)
+        url = urljoin(self.base_url, "token")
+        payload = {"username": credentials["username"], "password": credentials["password"], "grant_type": "password"}
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        payload = urlencode(payload)
+        response = self.session.post(url, data=payload, headers=headers)
+        response.raise_for_status()
+
+        return response.json()["access_token"]
+
+    def _token_is_valid(self, token: dict, current_timestamp: int) -> bool:
+        if isinstance(token["timestamp"], list):
+            token_timestamp = token["timestamp"][0]
+        else:
+            token_timestamp = token["timestamp"]
+
+        return current_timestamp - token_timestamp < self.oauth_token_timeout
+
+    def get_token(self):
+        have_valid_token = False
+        current_timestamp = arrow.utcnow().int_timestamp
+        token = ""
+        try:
+            cached_token = json.loads(self.token_store.get(self.scheme_slug))
+            try:
+                if self._token_is_valid(cached_token, current_timestamp):
+                    have_valid_token = True
+                    token = cached_token[f"{self.scheme_slug.replace('-', '_')}_access_token"]
+            except (KeyError, TypeError) as e:
+                log.exception(e)
+        except (KeyError, self.token_store.NoSuchToken):
+            pass
+
+        if not have_valid_token:
+            token = self._refresh_token()
+            self._store_token(token, current_timestamp)
+
+        return f"Bearer {token}"
+
+    def post(self, endpoint: str, body: dict = None, *, name: str) -> requests.models.Response:
+        auth_token = self.get_token()
+        headers = {"Authorization": auth_token}
+        log.debug(f"Posting {name} request with parameters: {body}.")
+        url = f"{self.base_url}{endpoint}"
+        response = self.session.post(url, json=body, headers=headers)
+        response.raise_for_status()
+        return response
+
+    def post_matched_transaction(self, body: dict, endpoint: str) -> requests.models.Response:
+        return self.post(endpoint, body, name="post_matched_transaction")
