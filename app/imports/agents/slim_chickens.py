f4ea9fd622be7d011496983ce889cf3a91cff952
backwardspy <backwardspy@pigeon.life>
2023-08-21 14:40:55 +0100

    fix: correctly convert payment card type in slims import agent

--- app/imports/agents/slim_chickens.py
+++ app/imports/agents/slim_chickens.py
@@ -6,7 +6,10 @@ from app.imports.agents.bases.queue_agent import QueueAgent
 
 PROVIDER_SLUG = "slim-chickens"
 
-SUPPORTED_PAYMENT_CARD_TYPES = ["VISA", "MASTER"]
+PAYMENT_CARD_TYPES = {
+    "VISA": "visa",
+    "MASTER": "mastercard",
+}
 
 
 class SlimChickens(QueueAgent):
@@ -37,12 +40,13 @@ class SlimChickens(QueueAgent):
     def _do_import(self, body: dict) -> None:
         txid = self.get_transaction_id(body)
 
-        is_supported_card_type = body["payment_card_type"] in SUPPORTED_PAYMENT_CARD_TYPES
+        is_supported_card_type = body["payment_card_type"] in PAYMENT_CARD_TYPES
 
         if not is_supported_card_type:
+            supported_types = ", ".join(PAYMENT_CARD_TYPES.keys())
             self.log.warning(
                 f"Discarding transaction {txid} due to unsupported payment card type {body['payment_card_type']!r}, "
-                f"expected one of {SUPPORTED_PAYMENT_CARD_TYPES!r}",
+                f"expected one of: {supported_types}",
             )
             return
 
@@ -63,7 +67,7 @@ class SlimChickens(QueueAgent):
     def to_transaction_fields(self, data: dict) -> SchemeTransactionFields:
         return SchemeTransactionFields(
             merchant_slug=self.provider_slug,
-            payment_provider_slug=data["payment_card_type"],
+            payment_provider_slug=PAYMENT_CARD_TYPES[data["payment_card_type"]],
             transaction_date=data["date"],
             has_time=True,
             spend_amount=data["amount"],
