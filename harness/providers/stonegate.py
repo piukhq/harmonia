7d67db0b4b5906575fb9c73a17f9d98f19b5a780
Bilal Leadbitter <32436173+Mylus1@users.noreply.github.com>
2023-10-02 15:16:07 +0100

    feat: :sparkles: Import retailer transactions for Stonegate (#166)
    * feat: :sparkles: Import retailer transactions for Stonegate
    
    * feat: :sparkles: Add stonegate to harness and registry
    
    * feat: add e2e fixture for stonegate
    
    ---------
    
    Co-authored-by: backwardspy <backwardspy@pigeon.life>

---
+++ harness/providers/stonegate.py
@@ -0,0 +1,27 @@
+import typing as t
+from uuid import uuid4
+
+from harness.providers.base import BaseImportDataProvider
+
+PAYMENT_CARD_TYPES = {"visa": "VS", "mastercard": "MC", "amex": "AX"}
+
+
+class Stonegate(BaseImportDataProvider):
+    def provide(self, fixture: dict) -> t.List[dict]:
+        return [
+            {
+                "transaction_id": str(uuid4()),
+                "payment_card_type": PAYMENT_CARD_TYPES[fixture["payment_provider"]["slug"]],
+                "payment_card_last_four": user["last_four"],
+                "amount": transaction["amount"],
+                "currency_code": "GBP",
+                "auth_code": transaction["auth_code"],
+                "date": transaction["date"],
+                "retailer_location_id": transaction["location_id"],
+                "metadata": {
+                    "account_id": str(uuid4()),
+                },
+            }
+            for user in fixture["users"]
+            for transaction in user["transactions"]
+        ]
