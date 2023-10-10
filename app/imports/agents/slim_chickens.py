34b2c54323f6762cbd17002045a35049672020eb
backwardspy (cl) <cl@bink.com>
2023-10-10 15:23:31 +0100

    feat: ignore case in slims payment feed dict (#169)

--- app/imports/agents/slim_chickens.py
+++ app/imports/agents/slim_chickens.py
@@ -9,8 +9,10 @@ from app.imports.agents.bases.queue_agent import QueueAgent
 PROVIDER_SLUG = "slim-chickens"
 
 PAYMENT_CARD_TYPES = {
-    "VISA": "visa",
-    "MASTER": "mastercard",
+    "visa debit": "visa",
+    "visa": "visa",
+    "mastercard": "mastercard",
+    "debit mastercard": "mastercard",
 }
 
 
@@ -42,13 +44,13 @@ class SlimChickens(QueueAgent):
     def _do_import(self, body: dict) -> None:
         txid = self.get_transaction_id(body)
 
-        is_supported_card_type = body["payment_card_type"] in PAYMENT_CARD_TYPES
+        is_supported_card_type = body["payment_card_type"].casefold() in PAYMENT_CARD_TYPES
 
         if not is_supported_card_type:
-            supported_types = ", ".join(PAYMENT_CARD_TYPES.keys())
+            supported_types = ", ".join(key.title() for key in PAYMENT_CARD_TYPES.keys())
             self.log.warning(
                 f"Discarding transaction {txid} due to unsupported payment card type {body['payment_card_type']!r}, "
-                f"expected one of: {supported_types}",
+                f"expected one of: {supported_types} (case insensitive)",
             )
             return
 
@@ -69,7 +71,7 @@ class SlimChickens(QueueAgent):
     def to_transaction_fields(self, data: dict) -> SchemeTransactionFields:
         return SchemeTransactionFields(
             merchant_slug=self.provider_slug,
-            payment_provider_slug=PAYMENT_CARD_TYPES[data["payment_card_type"]],
+            payment_provider_slug=PAYMENT_CARD_TYPES[data["payment_card_type"].casefold()],
             transaction_date=pendulum.instance(data["date"]),
             has_time=True,
             spend_amount=data["amount"],
